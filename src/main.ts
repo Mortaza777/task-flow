9300424622c47845b483013737d9da33bc4c127a
Mortaza <mortazahaydari081@gmail.com>
2026-05-11 15:30:43 +0430

    commit

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://127.0.0.1:5500', // یا '*'
+    origin: 'http://127.0.0.1:5500',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
